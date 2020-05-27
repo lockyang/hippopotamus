a59f5923f7d633d631c8536d3e0ad146058c6a7e
杨高尚 <yanggaoshang@qutoutiao.net>
2020-05-27 18:08:04 +0800

    🚧 [develop]demo

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
+//  dev test
 const inquirer = require('inquirer');
 const childProcess = require('child_process');
 const util = require('util');
 const exec = util.promisify(childProcess.exec);
 const git = require('./lib/git');
 const { catchError } = require('./lib/helper');
-
 const test = async () => {
   try {
     const t = await git.push()
